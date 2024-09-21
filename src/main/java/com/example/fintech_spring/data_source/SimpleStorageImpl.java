1b5dd619cfbf4141e4309e2704c8575e6ccdbc2f
lesam19092 <danigpro1337@gmail.com>
2024-09-21 22:53:14 +0300

    Добавил data source через hashmap

---
+++ src/main/java/com/example/fintech_spring/data_source/SimpleStorageImpl.java
@@ -0,0 +1,67 @@
+package com.example.fintech_spring.data_source;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Component
+public class SimpleStorageImpl<K, V> implements SimpleStorage<K, V> {
+
+    private final Map<K, V> storage = new HashMap<>();
+
+    @Override
+    public void put(K key, V value) {
+        storage.put(key, value);
+    }
+
+    @Override
+    public V get(K key) {
+        return storage.get(key);
+    }
+
+    @Override
+    public boolean remove(K key) {
+        return storage.remove(key) != null;
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return storage.containsKey(key);
+    }
+
+    @Override
+    public Map<K, V> getStorage() {
+        return storage;
+    }
+
+    @Override
+    public List<V> getList() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public void soutData() {
+        storage.forEach((k, v) -> {
+            System.out.printf("Key: %s, Value: %s%n", k, v);
+        });
+    }
+
+    @Override
+    public boolean update(K k, V v) {
+        if (storage.containsKey(k)) {
+            storage.put(k, v);
+            return true;
+        } else {
+            storage.put(k, v);
+            return true;
+        }
+    }
+
+}
+
