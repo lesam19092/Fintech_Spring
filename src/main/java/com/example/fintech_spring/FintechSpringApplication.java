e7b64555a7e8b1b62ca428d53c01f72b781bf9b9
lesam19092 <danigpro1337@gmail.com>
2024-09-21 22:43:55 +0300

    init

---
+++ src/main/java/com/example/fintech_spring/FintechSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintech_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechSpringApplication.class, args);
+    }
+
+}
