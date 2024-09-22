7b60f066e2a503352360cd81a956abf69c8cd1a5
lesam19092 <danigpro1337@gmail.com>
2024-09-23 00:04:09 +0300

    добавил uuid id

--- src/main/java/com/example/fintech_spring/dto/Location.java
+++ src/main/java/com/example/fintech_spring/dto/Location.java
@@ -4,12 +4,13 @@ package com.example.fintech_spring.dto;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Location implements Serializable {
-
+    private UUID uuid;
     private String slug;
     private String name;
 
