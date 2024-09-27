bb8ee8a9598dbea72359a1b75af7459fbdd46f08
lesam19092 <danigpro1337@gmail.com>
2024-09-28 02:10:39 +0300

    замокал дата соурс

--- src/test/java/com/example/fintech_spring/data_source/RepositoryImplTest.java
+++ src/test/java/com/example/fintech_spring/data_source/RepositoryImplTest.java
@@ -1,41 +1,40 @@
 package com.example.fintech_spring.data_source;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class RepositoryImplTest {
 
-    private Repository<String, Integer> repository;
+    @Mock
+    private RepositoryImpl<String, Integer> repository;
 
-    @BeforeEach
-    public void setUp() {
-        repository = new RepositoryImpl<>();
-    }
 
     @Test
-    public void testSave_ShouldStoreKeyValue() {
+    void testSave_ShouldStoreKeyValue() {
         String key = "testKey";
         Integer value = 10;
-
-        repository.save(key, value);
-
-        assertEquals(value, repository.findById(key).get());
+        when(repository.findById(key)).thenReturn(Optional.of(value));
+        var a = repository.findById(key).get();
+        assertEquals(value, a);
     }
 
     @Test
     public void testFindById_ExistingKey_ShouldReturnOptionalWithValue() {
         String key = "existingKey";
         Integer value = 20;
-
-        repository.save(key, value);
-
+        when(repository.findById(key)).thenReturn(Optional.of(value));
         Optional<Integer> optionalValue = repository.findById(key);
-
         assertAll(
                 () -> {
                     assertTrue(optionalValue.isPresent());
@@ -47,59 +46,91 @@ class RepositoryImplTest {
 
     @Test
     public void testFindById_NonExistingKey_ShouldReturnOptionalEmpty() {
-        String key = "nonExistingKey";
-
-        Optional<Integer> optionalValue = repository.findById(key);
+        String nonExistentKey = "nonExistingKey";
+        when(repository.findById(nonExistentKey)).thenReturn(Optional.empty());
+        Optional<Integer> optionalValue = repository.findById(nonExistentKey);
         assertFalse(optionalValue.isPresent());
     }
 
     @Test
-    public void testDeleteById_ExistingKey_ShouldReturnTrue() {
-        String key = "deleteKey";
+    void testDeleteById_ExistingKey_ShouldReturnTrue() {
+        String existingKey = "deleteKey";
         Integer value = 30;
 
-        repository.save(key, value);
+        when(repository.deleteById(existingKey)).thenReturn(true);
 
-        boolean deleted = repository.deleteById(key);
+        boolean deleted = repository.deleteById(existingKey);
 
         assertAll(
                 () -> {
                     assertTrue(deleted);
-                    assertFalse(repository.findById(key).isPresent());
+                    verify(repository).deleteById(existingKey);
+                    assertFalse(repository.findById(existingKey).isPresent());
+                }
+        );
+    }
+
+    @Test
+    void testDeleteById_NonExistingKey_ShouldReturnFalse() {
+        String nonExistingKey = "notThere";
+
+        when(repository.deleteById(nonExistingKey)).thenReturn(false);
+
+        boolean deleted = repository.deleteById(nonExistingKey);
+
+        assertAll(
+                () -> {
+                    assertFalse(deleted);
+                    verify(repository).deleteById(nonExistingKey);
+
                 }
         );
 
     }
 
     @Test
-    public void testDeleteById_NonExistingKey_ShouldReturnFalse() {
-        String key = "notThere";
-
-        boolean deleted = repository.deleteById(key);
-        assertFalse(deleted);
-    }
-
-    @Test
-    public void testExistsById_ExistingKey_ShouldReturnTrue() {
-        String key = "exists";
+    void testExistsById_ExistingKey_ShouldReturnTrue() {
+        String existingKey = "exists";
         Integer value = 40;
+        when(repository.existsById(existingKey)).thenReturn(true);
+        boolean exists = repository.existsById(existingKey);
 
-        repository.save(key, value);
+        assertAll(
+                () -> {
+                    assertTrue(exists);
+                    verify(repository).existsById(existingKey);
 
-        assertTrue(repository.existsById(key));
+                }
+        );
     }
 
     @Test
-    public void testExistsById_NonExistingKey_ShouldReturnFalse() {
-        String key = "missing";
+    void testExistsById_NonExistingKey_ShouldReturnFalse() {
+        String nonExistingKey = "missing";
+        when(repository.existsById(nonExistingKey)).thenReturn(false);
+        boolean exists = repository.existsById(nonExistingKey);
 
-        assertFalse(repository.existsById(key));
+        assertAll(
+                () -> {
+                    assertFalse(exists);
+                    verify(repository).existsById(nonExistingKey);
+
+                }
+        );
     }
 
     @Test
     public void testFindAll_EmptyRepository_ShouldReturnEmptyList() {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
         List<Integer> allValues = repository.findAll();
-        assertTrue(allValues.isEmpty());
+
+        assertAll(
+                () -> {
+                    assertTrue(allValues.isEmpty());
+                    verify(repository).findAll();
+
+                }
+        );
     }
 
     @Test
@@ -108,20 +139,19 @@ class RepositoryImplTest {
         Integer value1 = 50;
         String key2 = "key2";
         Integer value2 = 60;
-
+        when(repository.findAll()).thenReturn(Arrays.asList(value1, value2));
         repository.save(key1, value1);
         repository.save(key2, value2);
-
         List<Integer> allValues = repository.findAll();
-
         assertAll(
                 () -> {
                     assertEquals(2, allValues.size());
                     assertTrue(allValues.contains(value1));
                     assertTrue(allValues.contains(value2));
+                    verify(repository, times(1)).findAll();
+
                 }
         );
-
     }
 
     @Test
@@ -130,16 +160,33 @@ class RepositoryImplTest {
         Integer value1 = 70;
         Integer value2 = 80;
 
+        when(repository.findById(key)).thenReturn(Optional.of(value2));
+
         repository.save(key, value1);
         repository.update(key, value2);
+        Optional<Integer> updatedValue = repository.findById(key);
 
-        assertEquals(value2, repository.findById(key).get());
+        assertAll(
+                () -> {
+                    assertTrue(updatedValue.isPresent());
+                    assertEquals(value2, updatedValue.get());
+                    verify(repository, times(1)).findById(key);
+                    verify(repository, times(1)).update(key, value2);
+                }
+        );
     }
 
     @Test
     public void testGetTotalCount_EmptyRepository_ShouldReturnZero() {
+        when(repository.getTotalCount()).thenReturn(0);
         int totalCount = repository.getTotalCount();
-        assertEquals(0, totalCount);
+
+        assertAll(
+                () -> {
+                    assertEquals(0, totalCount);
+                    verify(repository, times(1)).getTotalCount();
+                }
+        );
     }
 
     @Test
@@ -149,10 +196,18 @@ class RepositoryImplTest {
         String key2 = "key4";
         Integer value2 = 100;
 
+        when(repository.getTotalCount()).thenReturn(2);
+
         repository.save(key1, value1);
         repository.save(key2, value2);
-
         int totalCount = repository.getTotalCount();
-        assertEquals(2, totalCount);
+
+        assertAll(
+                () -> {
+                    assertEquals(2, totalCount);
+                    verify(repository, times(1)).getTotalCount();
+                }
+        );
+
     }
 }
