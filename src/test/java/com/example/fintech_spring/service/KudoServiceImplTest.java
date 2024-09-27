abb91b777fb24ed551a6316ba263df43662bbf12
lesam19092 <danigpro1337@gmail.com>
2024-09-28 02:11:09 +0300

    написал через wiremock тесты

--- src/test/java/com/example/fintech_spring/service/KudoServiceImplTest.java
+++ src/test/java/com/example/fintech_spring/service/KudoServiceImplTest.java
@@ -1,55 +1,110 @@
 package com.example.fintech_spring.service;
 
+
 import com.example.fintech_spring.dto.Category;
 import com.example.fintech_spring.dto.Location;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
-import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class KudoServiceImplTest {
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WireMockTest
+@AutoConfigureMockMvc
+public class KudoServiceImplTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
 
-    @Test
-    public void fetchingCategories() {
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort().dynamicPort())
+            .build();
 
-        ResponseEntity<List<Category>> response = restTemplate.exchange("https://kudago.com/public-api/v1.4/place-categories", HttpMethod.GET, null,
-                new ParameterizedTypeReference<>() {
-                });
-
-        List<Category> category = response.getBody();
-
-        assertThat(category, hasSize(54));
-
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("https://kudago.com/public-api/v1.4", wireMockServer::baseUrl);
     }
 
 
     @Test
-    void fetchingLocations() {
-        ResponseEntity<List<Location>> response = restTemplate.exchange("https://kudago.com/public-api/v1.4/locations", HttpMethod.GET, null,
-                new ParameterizedTypeReference<>() {
-                });
-
-        List<Location> locations = response.getBody();
-
-        assertThat(locations, hasSize(5));
+    public void fetchingCategories() throws Exception {
+        String expectedJson = mapper
+                .writeValueAsString(new Category(1, "slug1", "name1"));
+        wireMockServer.stubFor(
+                WireMock.get(urlEqualTo("/place-categories"))
+                        .willReturn(aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody(expectedJson))
+        );
+        var data = getContent(wireMockServer.baseUrl() + "/place-categories");
+        Assertions.assertEquals(data.get(), expectedJson);
     }
+
+    @Test
+    public void fetchingLocations() throws Exception {
+        String expectedJson = mapper
+                .writeValueAsString(new Location(UUID.randomUUID(), "slug1", "name1"));
+        wireMockServer.stubFor(
+                WireMock.get(urlEqualTo("/locations"))
+                        .willReturn(aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody(expectedJson))
+        );
+        var data = getContent(wireMockServer.baseUrl() + "/locations");
+        Assertions.assertEquals(data.get(), expectedJson);
+    }
+
+
+    @Test
+    public void testGetAllCategoriesError() {
+        wireMockServer.stubFor(get(urlEqualTo("/place-categories"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+        var data = getContent(wireMockServer.baseUrl() + "/place-categories");
+        Assertions.assertTrue(data.isEmpty());
+
+    }
+
+    @Test
+    public void testGetAllLocationsError() {
+        wireMockServer.stubFor(get(urlEqualTo("/locations"))
+                .willReturn(aResponse()
+                        .withStatus(500)));
+        var data = getContent(wireMockServer.baseUrl() + "/locations");
+        Assertions.assertTrue(data.isEmpty());
+    }
+
+
+    private Optional<String> getContent(String url) {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        ResponseEntity<String> rateResponse =
+                testRestTemplate.exchange(url,
+                        HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+                        });
+
+        return Optional.ofNullable(rateResponse.getBody());
+    }
+
 }
