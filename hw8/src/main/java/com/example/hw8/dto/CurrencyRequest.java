284ef521c7cc9704e4371cbcf0e3a975125cbaa8
lesam19092 <danigpro1337@gmail.com>
2024-10-08 23:10:42 +0300

    добавил dto

--- hw8/src/main/java/com/example/hw8/dto/CurrencyRequest.java
+++ hw8/src/main/java/com/example/hw8/dto/CurrencyRequest.java
@@ -2,7 +2,6 @@ package com.example.hw8.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
