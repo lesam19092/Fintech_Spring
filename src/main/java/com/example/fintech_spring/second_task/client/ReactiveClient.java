f13d5951549d233e3eb7bad72f486741aafd0469
lesam19092 <danigpro1337@gmail.com>
2024-10-14 13:06:02 +0300

    отрефакторил код , убрал некоторые методы в ClientUtils

--- src/main/java/com/example/fintech_spring/second_task/client/ReactiveClient.java
+++ src/main/java/com/example/fintech_spring/second_task/client/ReactiveClient.java
@@ -1,37 +1,35 @@
 package com.example.fintech_spring.second_task.client;
 
+import com.example.fintech_spring.configuration.ReactiveConfiguration;
 import com.example.fintech_spring.second_task.dto.ApiEvent;
 import com.example.fintech_spring.second_task.dto.CurrencyRequest;
 import com.example.fintech_spring.second_task.dto.CurrencyResponse;
 import com.example.fintech_spring.second_task.dto.Event;
+import com.example.fintech_spring.second_task.utils.ClientUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+import static com.example.fintech_spring.second_task.utils.ClientUtils.getUrl;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@ConditionalOnProperty(value = "api.client", havingValue = "reactive", matchIfMissing = false)
+@ConditionalOnBean(ReactiveConfiguration.class)
 public class ReactiveClient implements ApiClient {
 
     private final WebClient webClientForEvents;
     private final WebClient webClientForConvertValute;
-    private static final ZoneOffset MOSCOW_TIME_ZONE = ZoneOffset.ofHours(+3);
-
 
     @Override
     public List<Event> getEvents(LocalDate dateFrom, LocalDate dateTo) {
-
         log.info("Fetching events from {} to {}", dateFrom, dateTo);
 
         return webClientForEvents.get()
@@ -39,7 +37,7 @@ public class ReactiveClient implements ApiClient {
                 .retrieve()
                 .bodyToMono(ApiEvent.class)
                 .map(ApiEvent::getResults)
-                .map(this::setEventPrices)
+                .map(ClientUtils::setEventPrices)
                 .doOnNext(events -> log.info("Fetched {} events", events.size())).block();
 
     }
@@ -60,42 +58,6 @@ public class ReactiveClient implements ApiClient {
     }
 
 
-    private long getActualSince(LocalDate dateFrom) {
-        return dateFrom != null ?
-                dateFrom.atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE) :
-                LocalDate.now().minusDays(7).atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE);
-    }
-
-    private long getActualUntil(LocalDate dateTo) {
-        return dateTo != null ?
-                dateTo.atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE) :
-                LocalDate.now().atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE);
-    }
-
-    private String getUrl(LocalDate dateFrom, LocalDate dateTo) {
-        long actual_since = getActualSince(dateFrom);
-        long actual_until = getActualUntil(dateTo);
-        String url = String.format("/events/?location=spb&fields=price,id,title,dates&actual_since=%d&actual_until=%d",
-                actual_since, actual_until);
-        log.debug("Generated URL: {}", url);
-        return url;
-    }
-
-    private List<Event> setEventPrices(List<Event> events) {
-        log.debug("Setting prices for events");
-        events.forEach(event -> {
-            Pattern pattern = Pattern.compile("^\\D*(\\d+)");
-            Matcher matcher = pattern.matcher(event.getPrice());
-            if (matcher.find()) {
-                event.setPrice(matcher.group(1));
-            } else {
-                event.setPrice("0");
-            }
-        });
-        return events;
-    }
-
-
     public Mono<List<Event>> getEventsMono(LocalDate dateFrom, LocalDate dateTo) {
 
         log.info("Fetching events from {} to {}", dateFrom, dateTo);
@@ -105,7 +67,7 @@ public class ReactiveClient implements ApiClient {
                 .retrieve()
                 .bodyToMono(ApiEvent.class)
                 .map(ApiEvent::getResults)
-                .map(this::setEventPrices)
+                .map(ClientUtils::setEventPrices)
                 .doOnNext(events -> log.info("Fetched {} events", events.size()));
 
     }
@@ -123,5 +85,6 @@ public class ReactiveClient implements ApiClient {
                 .map(CurrencyResponse::getConvertedAmount)
                 .doOnNext(amount -> log.info("Converted amount: {}", amount));
     }
+
 
 }
--- src/main/java/com/example/fintech_spring/second_task/client/RestTemplateClient.java
+++ src/main/java/com/example/fintech_spring/second_task/client/RestTemplateClient.java
@@ -1,15 +1,16 @@
 package com.example.fintech_spring.second_task.client;
 
+import com.example.fintech_spring.configuration.RestTemplateConfiguration;
 import com.example.fintech_spring.second_task.dto.ApiEvent;
 import com.example.fintech_spring.second_task.dto.CurrencyRequest;
 import com.example.fintech_spring.second_task.dto.CurrencyResponse;
 import com.example.fintech_spring.second_task.dto.Event;
+import com.example.fintech_spring.second_task.utils.ClientUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
@@ -20,21 +21,19 @@ import java.util.regex.Pattern;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@ConditionalOnProperty(value = "api.client", havingValue = "non-reactive", matchIfMissing = true)
+@ConditionalOnBean(RestTemplateConfiguration.class)
 public class RestTemplateClient implements ApiClient {
 
     private final RestTemplate restTemplateForEvents;
     private final RestTemplate restTemplateForConvertValute;
 
-    private static final ZoneOffset MOSCOW_TIME_ZONE = ZoneOffset.ofHours(+3);
-
 
     @Override
     public List<Event> getEvents(LocalDate dateFrom, LocalDate dateTo) {
-        ApiEvent apiEvent = restTemplateForEvents.getForObject(getUrl(dateFrom, dateTo), ApiEvent.class);
+        ApiEvent apiEvent = restTemplateForEvents.getForObject(ClientUtils.getUrl(dateFrom, dateTo), ApiEvent.class);
         List<Event> events = apiEvent.getResults();
         log.debug("Fetched {} events", events.size());
-        return setEventPrices(events);
+        return ClientUtils.setEventPrices(events);
     }
 
     @Override
@@ -47,40 +46,5 @@ public class RestTemplateClient implements ApiClient {
                 .postForObject("/currencies/convert", currencyRequest, CurrencyResponse.class);
         log.info("Converted amount: {}", response.getConvertedAmount());
         return response.getConvertedAmount();
-    }
-
-    private long getActualSince(LocalDate dateFrom) {
-        return dateFrom != null ?
-                dateFrom.atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE) :
-                LocalDate.now().minusDays(7).atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE);
-    }
-
-    private long getActualUntil(LocalDate dateTo) {
-        return dateTo != null ?
-                dateTo.atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE) :
-                LocalDate.now().atStartOfDay().toEpochSecond(MOSCOW_TIME_ZONE);
-    }
-
-    private String getUrl(LocalDate dateFrom, LocalDate dateTo) {
-        long actual_since = getActualSince(dateFrom);
-        long actual_until = getActualUntil(dateTo);
-        String url = String.format("/events/?location=spb&fields=price,id,title,dates&actual_since=%d&actual_until=%d",
-                actual_since, actual_until);
-        log.debug("Generated URL: {}", url);
-        return url;
-    }
-
-    private List<Event> setEventPrices(List<Event> events) {
-        log.debug("Setting prices for events");
-        events.forEach(event -> {
-            Pattern pattern = Pattern.compile("^\\D*(\\d+)");
-            Matcher matcher = pattern.matcher(event.getPrice());
-            if (matcher.find()) {
-                event.setPrice(matcher.group(1));
-            } else {
-                event.setPrice("0");
-            }
-        });
-        return events;
     }
 }
